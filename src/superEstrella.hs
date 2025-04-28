f203be8ddb92ce6fb8012fe5461ceea28c1802fc
ValentinaTavellaSassali <vtavellasassali@frba.utn.edu.ar>
2025-04-28 12:13:40 -0300

    agrego personajeespina

--- src/superEstrella.hs
+++ src/superEstrella.hs
@@ -1,8 +1,22 @@
-data Personaje = UnPersonaje {
-    nombre :: String
-    poderBasico :: String
-    superPoder :: String
-    poderActivo :: Bool
+data Personaje = Personaje {
+    nombre :: String,
+    basicPower :: String,
+    superPower :: String,
+    poderActivo :: Bool,
     cantidadDeVida :: Int
 }
 
+personajeEspina :: Personaje
+personajeEspina unPersonaje = unPersonaje {
+    nombre = "Espina",
+    basicPower = "bolaEspinosa",
+    superPower = "granadaDeEspinas",
+    poderActivo = False,
+    cantidadDeVida = 1000
+}
+
+vidaDePersonaje :: Personaje -> Int
+vidaDePersonaje unPersonaje = unPersonaje.cantidadDeVida
+
+bolaEspinosa :: Personaje -> Personaje
+bolaEspinosa unPersonaje = 
