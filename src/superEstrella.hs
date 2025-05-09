976bdd49dd20380f439f56b4132c37afebf645e2
ValentinaTavellaSassali <vtavellasassali@frba.utn.edu.ar>
2025-05-09 13:39:14 -0300

    agrego varias cosas

--- src/superEstrella.hs
+++ src/superEstrella.hs
@@ -1,3 +1,4 @@
+
 data Personaje = Personaje {
     nombre :: String,
     basicPower :: String,
@@ -6,22 +7,56 @@ data Personaje = Personaje {
     cantidadDeVida :: Int
 } deriving (Show)
 
-personajeEspina :: Personaje
-personajeEspina = Personaje {
+espina :: Personaje
+espina = Personaje {
     nombre = "Espina",
     basicPower = "bolaEspinosa",
     superPower = "granadaDeEspinas",
-    poderActivo = False,
-    cantidadDeVida = 10000
+    poderActivo = True,
+    cantidadDeVida = 4800
 }
 
-personajePamela :: Personaje
-personajePamela = Personaje {
+pamela :: Personaje
+pamela = Personaje {
     nombre = "Pamela",
     basicPower = "lluviaDeTuercas",
     superPower = "torretaCurativa",
     poderActivo = False,
-    cantidadDeVida = 10000
+    cantidadDeVida = 9600
 }
 
+bolaEspinosa :: Personaje -> Personaje
+bolaEspinosa unPersonaje
+  | cantidadDeVida unPersonaje > 1000 = unPersonaje {
+        cantidadDeVida = cantidadDeVida unPersonaje - 1000
+    }
+  | otherwise = unPersonaje {
+        cantidadDeVida = 0
+    }
 
+lluviaDeTuercas :: Personaje -> String -> Personaje
+lluviaDeTuercas unPersonaje tipo
+  | tipo == "sanador" = unPersonaje {
+        cantidadDeVida = cantidadDeVida unPersonaje + 800
+        }
+  | tipo == "dañino" = unPersonaje {
+        cantidadDeVida = cantidadDeVida unPersonaje `div` 2
+        }
+  | otherwise = unPersonaje
+
+granadaDeEspinas :: Personaje -> Int -> Personaje
+granadaDeEspinas unPersonaje radioExplosion
+  | radioExplosion > 3 = unPersonaje {
+        nombre = nombre unPersonaje ++ " Espina estuvo aquí"
+    }
+  | radioExplosion > 3 && cantidadDeVida unPersonaje < 800 = unPersonaje {
+        poderActivo = False,
+        cantidadDeVida = 0
+    }
+  | otherwise = bolaEspinosa unPersonaje
+
+torretaCurativa :: Personaje -> Personaje
+torretaCurativa unPersonaje = unPersonaje {
+    poderActivo = True,
+    cantidadDeVida = cantidadDeVida unPersonaje * 2
+}
